2f1268bba917bcb3f12509ce4708b52f139c6209
Shazzad Hossain Mazumder <shmazumder23@gmail.com>
2026-07-17 21:01:47 +0600

    Update update_nb.py

--- scripts/update_nb.py
+++ scripts/update_nb.py
@@ -3,18 +3,14 @@ import json
 with open("notebooks/Mamba3Yolo_Kaggle_Runbook.ipynb", "r", encoding="utf-8") as f:
     nb = json.load(f)
 
-with open("src/blocks/mamba3_ref.py", "r", encoding="utf-8") as f:
-    ref_code = "%%writefile src/blocks/mamba3_ref.py\n" + f.read()
-    
-with open("src/blocks/mamba3_odss.py", "r", encoding="utf-8") as f:
-    odss_code = "%%writefile src/blocks/mamba3_odss.py\n" + f.read()
+with open("scripts/validate_core.py", "r", encoding="utf-8") as f:
+    validate_code = "%%writefile scripts/validate_core.py\n" + f.read()
 
 def to_lines(text):
     lines = text.split('\n')
     return [l + '\n' for l in lines[:-1]] + [lines[-1]] if lines else []
 
-nb['cells'][6]['source'] = to_lines(ref_code)
-nb['cells'][7]['source'] = to_lines(odss_code)
+nb['cells'][10]['source'] = to_lines(validate_code)
 
 with open("notebooks/Mamba3Yolo_Kaggle_Runbook.ipynb", "w", encoding="utf-8") as f:
     json.dump(nb, f, indent=1, ensure_ascii=False)
